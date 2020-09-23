c2eadd0edb58210bb2177444f9fef715ace10335
Alfian SS <alfian.m@softwareseni.com>
2020-09-23 16:22:39 +0700

    update display postmeta

--- team-members.php
+++ team-members.php
@@ -41,7 +41,7 @@ class Team_Members {
     }
 
     public function team_members_admin() {
-        add_meta_box( 'movie_review_meta_box',
+        add_meta_box( 'team_members_meta_box',
             'Other Information',
             array($this, 'display_team_members_meta_box'),
             'team_members', 'normal', 'high'
@@ -76,8 +76,8 @@ class Team_Members {
         $tm_email    = esc_html( get_post_meta( $team_members->ID, 'tm_email', true ) );
         $tm_phone    = esc_html( get_post_meta( $team_members->ID, 'tm_phone', true ) );
         $tm_url      = esc_html( get_post_meta( $team_members->ID, 'tm_url', true ) );
-        $tm_image    = get_post_meta( $team_members->ID, 'tm_images', true );
-
+        $tm_image    = get_post_meta( $team_members->ID, 'tm_images', true );        
+        
         echo "<table>
                 <tr style='width:100%;'>
                     <td style='width:50%;'>Position</td>
@@ -97,17 +97,44 @@ class Team_Members {
                 </tr>
                 <tr style='width:100%;'>
                     <td style='width:50%;'>Image</td>
-                    <td style='width:50%;'>
-                        <input type='File' name='tm_images' >
-                    </td>
+                    <td style='width:50%;'>";
+                    if(isset($tm_image) && $tm_image != '') {
+                        echo "<input type='text' name='tm_images' value='".$tm_image."'>";
+                    } else {
+                        echo "<input type='file' name='tm_images' >";
+                    }
+                    "</td>
                 </tr>
                 </table>";
     }
 
+    public function tm_shortcode() {
+        $mypost = array( 'post_type' => 'team_members', );
+        $loop = new WP_Query( $mypost );
+
+        $html = "<div class='row col-md-12' style='width: 100%; '>";
+        while ( $loop->have_posts() ) : $loop->the_post();;
+            $html .= "<div class='col-md-3' style='float:left; padding: 0 5px;'>";
+            $html .= "<img src='".esc_html( get_post_meta( get_the_ID(), 'tm_images', true ) )."'>";
+            $html .= "<span>".esc_html( get_post_meta( get_the_ID(), 'tm_position', true ) )."</span><br>";
+            $html .= "<span>".esc_html( get_post_meta( get_the_ID(), 'tm_email', true ) )."</span><br>";
+            $html .= "<span>".esc_html( get_post_meta( get_the_ID(), 'tm_phone', true ) )."</span><br>";
+            $html .= "<span>".esc_html( get_post_meta( get_the_ID(), 'tm_url', true ) )."</span><br>";
+            $html .= "</div>";
+        endwhile;
+        wp_reset_query();
+        $html .= "</div>";
+
+        echo $html;
+    }
+
+    
 }
 
 $team_members = new Team_Members();
 
 add_action( 'init', array($team_members, 'create_team_members') );
 add_action( 'admin_init', array($team_members, 'team_members_admin') );
-add_action( 'save_post', array($team_members, 'add_team_members_fields', 10, 2) );
+add_action( 'save_post', array($team_members, 'add_team_members_fields'), 10, 2 );
+
+add_shortcode( 'team_members_sc', array($team_members,'tm_shortcode') );
