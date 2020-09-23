6156031176df0825e57d23825469e567a4a9dc9c
Alfian SS <alfian.m@softwareseni.com>
2020-09-23 14:12:38 +0700

    update form add team members

--- team-members.php
+++ team-members.php
@@ -43,49 +43,13 @@ class Team_Members {
     public function team_members_admin() {
         add_meta_box( 'movie_review_meta_box',
             'Other Information',
-            array($this,'display_team_members_meta_box'),
+            array($this, 'display_team_members_meta_box'),
             'team_members', 'normal', 'high'
         );
     }
-
-    public function display_team_members_meta_box( $team_members ) {
-        // Retrieve current name of the Director and Movie Rating based on review ID
-        $tm_position = esc_html( get_post_meta( $team_members->ID, 'tm_position', true ) );
-        $tm_email    = esc_html( get_post_meta( $team_members->ID, 'tm_email', true ) );
-        $tm_phone    = esc_html( get_post_meta( $team_members->ID, 'tm_phone', true ) );
-        $tm_url      = esc_html( get_post_meta( $team_members->ID, 'tm_url', true ) );
-        $tm_image    = get_post_meta( $team_members->ID, 'movie_rating', true );
-
-        echo "<table>
-                <tr style='width:100%;'>
-                    <td style='width:50%;'>Position</td>
-                    <td style='width:50%;'><input type='text' name='tm_position'></td>
-                </tr>
-                <tr style='width:100%;'>
-                    <td style='width:50%;'>Email</td>
-                    <td style='width:50%;'><input type='text' name='tm_email'></td>
-                </tr>
-                <tr style='width:100%;'>
-                    <td style='width:50%;'>Phone</td>
-                    <td style='width:50%;'><input type='text' name='tm_phone'></td>
-                </tr>
-                <tr style='width:100%;'>
-                    <td style='width:50%;'>Website</td>
-                    <td style='width:50%;'><input type='text' name='tm_url'></td>
-                </tr>
-                <tr style='width:100%;'>
-                    <td style='width:50%;'>Image</td>
-                    <td style='width:50%;'>
-                        <img src=''>
-                        <input type='File' name='tm_images'>
-                    </td>
-                </tr>
-                </table>";
-    }
-
-
+    
     public function add_team_members_fields( $team_members_id, $team_members ) {
-        // Check post type for movie reviews
+        // Check post type for team members
         if ( $team_members->post_type == 'team_members' ) {
             // Store data in post meta table if present in post data
             if ( isset( $_POST['tm_position'] ) && $_POST['tm_position'] != '' ) {
@@ -94,16 +58,50 @@ class Team_Members {
             if ( isset( $_POST['tm_email'] ) && $_POST['tm_email'] != '' ) {
                 update_post_meta( $team_members_id, 'tm_email', $_POST['tm_email'] );
             }
-            if ( isset( $_POST['tm_position'] ) && $_POST['tm_position'] != '' ) {
-                update_post_meta( $team_members_id, 'tm_position', $_POST['tm_position'] );
+            if ( isset( $_POST['tm_phone'] ) && $_POST['tm_phone'] != '' ) {
+                update_post_meta( $team_members_id, 'tm_phone', $_POST['tm_phone'] );
             }
-            if ( isset( $_POST['tm_position'] ) && $_POST['tm_position'] != '' ) {
-                update_post_meta( $team_members_id, 'tm_position', $_POST['tm_position'] );
+            if ( isset( $_POST['tm_url'] ) && $_POST['tm_url'] != '' ) {
+                update_post_meta( $team_members_id, 'tm_url', $_POST['tm_url'] );
             }
-            if ( isset( $_POST['tm_position'] ) && $_POST['tm_position'] != '' ) {
-                update_post_meta( $team_members_id, 'tm_position', $_POST['tm_position'] );
+            if ( isset( $_POST['tm_images'] ) && $_POST['tm_images'] != '' ) {
+                update_post_meta( $team_members_id, 'tm_images', $_POST['tm_images'] );
             }
         }
+    }
+
+    public function display_team_members_meta_box( $team_members ) {
+        // Retrieve current name of the Director and Movie Rating based on review ID
+        $tm_position = esc_html( get_post_meta( $team_members->ID, 'tm_position', true ) );
+        $tm_email    = esc_html( get_post_meta( $team_members->ID, 'tm_email', true ) );
+        $tm_phone    = esc_html( get_post_meta( $team_members->ID, 'tm_phone', true ) );
+        $tm_url      = esc_html( get_post_meta( $team_members->ID, 'tm_url', true ) );
+        $tm_image    = get_post_meta( $team_members->ID, 'tm_images', true );
+
+        echo "<table>
+                <tr style='width:100%;'>
+                    <td style='width:50%;'>Position</td>
+                    <td style='width:50%;'><input type='text' name='tm_position' value='". $tm_position ."'></td>
+                </tr>
+                 <tr style='width:100%;'>
+                    <td style='width:50%;'>Email</td>
+                    <td style='width:50%;'><input type='text' name='tm_email' value='". $tm_email ."'></td>
+                </tr>
+                <tr style='width:100%;'>
+                    <td style='width:50%;'>Phone</td>
+                    <td style='width:50%;'><input type='text' name='tm_phone' value='". $tm_phone ."'></td>
+                </tr>
+                <tr style='width:100%;'>
+                    <td style='width:50%;'>Website</td>
+                    <td style='width:50%;'><input type='text' name='tm_url' value='". $tm_url ."'></td>
+                </tr>
+                <tr style='width:100%;'>
+                    <td style='width:50%;'>Image</td>
+                    <td style='width:50%;'>
+                        <input type='File' name='tm_images' >
+                    </td>
+                </tr>
+                </table>";
     }
 
 }
@@ -112,4 +110,4 @@ $team_members = new Team_Members();
 
 add_action( 'init', array($team_members, 'create_team_members') );
 add_action( 'admin_init', array($team_members, 'team_members_admin') );
-add_action( 'save_post', array($team_members, 'add_movie_review_fields', 10, 2) );
+add_action( 'save_post', array($team_members, 'add_team_members_fields', 10, 2) );
